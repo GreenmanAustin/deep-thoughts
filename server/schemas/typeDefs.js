74fd99afa7e2f4d720a9bc548e416d3a94239d47
Sheroo Bhagia <sheroo@sb-mac.lan>
2022-05-22 00:08:48 -0500

    code from canvas

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,9 +1,14 @@
-// import the gql tagged template function
 const { gql } = require('apollo-server-express');
 
-// create our typeDefs
-
 const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    friendCount: Int
+    thoughts: [Thought]
+    friends: [User]
+  }
 
   type Thought {
     _id: ID
@@ -20,15 +25,7 @@ const typeDefs = gql`
     createdAt: String
     username: String
   }
-  type User {
-    _id: ID
-    username: String
-    email: String
-    friendCount: Int
-    thoughts: [Thought]
-    friends: [User]
-  }
-  
+
   type Query {
     users: [User]
     user(username: String!): User
@@ -37,6 +34,4 @@ const typeDefs = gql`
   }
 `;
 
-
-// export the typeDefs
 module.exports = typeDefs;
